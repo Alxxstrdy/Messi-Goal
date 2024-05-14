ad0cfba05fa461179e54c95406331a4800f398fe
PUTRA <msabilshahputra@gmail.com>
2024-05-14 09:48:41 +0700

    CODEWARS

---
+++ src/messigoal/MESSIGOAL.java
@@ -0,0 +1,10 @@
+package messigoal;
+public class MESSIGOAL {
+class Goals {
+  public static final int laLigaGoals = 43;
+  public static final int championsLeagueGoals = 10;
+  public static final int copaDelReyGoals = 5;
+  
+  public static final int totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+    }
+}
